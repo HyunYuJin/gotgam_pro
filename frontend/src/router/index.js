85c26e63bf95490cfc2f9d30a6aac1311620af7a
YujinHyeon <>
2019-10-23 13:51:03 +0900

    Add Login Page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,24 +3,31 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-import Index from '../components/IndexPage'
-import Show from '../components/ShowPage'
-
+// import Index from '../components/IndexPage'
+// import Show from '../components/ShowPage'
+import MainPage from '../components/container.vue';
 import Login from '@/components/login.vue';
 
 export default new Router({
 //  mode: 'history',
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'index',
+    //   component: Index
+    // },
+    // {
+    //   path: '/:id',
+    //   name: 'show',
+    //   component: Show
+    // },
+
     {
       path: '/',
-      name: 'index',
-      component: Index
+      name: 'MainPage',
+      component: MainPage
     },
-    {
-      path: '/:id',
-      name: 'show',
-      component: Show
-    },
+    
     {
       path: '/login',
       name: 'Login',
