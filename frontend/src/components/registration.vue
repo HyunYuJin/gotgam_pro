5f5950cbbe3c89c2629f7e889a66991409c5ac19
YujinHyeon <>
2019-10-25 01:23:45 +0900

    Error

--- frontend/src/components/registration.vue
+++ frontend/src/components/registration.vue
@@ -1,123 +1,103 @@
 <template>
   <div class="registration_wrap">
-      <div class="registration_inner_wrap">
-          <!-- .container -->
-          <b-container>
-            <!-- .stepwizard -->
-            <b-col offset-md="12" class="stepwizard">
-                <div class="stepwizard-row setup-panel">
-                <div class="stepwizard-step">
-                    <b-button href="#step-1">1</b-button>
-                    <p>Step 1</p>
-                </div>
-                <div class="stepwizard-step">
-                    <b-button href="#step-2">2</b-button>
-                    <p>Step 2</p>
-                </div>
-                <div class="stepwizard-step">
-                    <b-button href="#step-3">3</b-button>
-                    <p>Step 3</p>
-                </div>
-                </div>
-            </b-col>
-            
+    <div class="registration_inner_wrap">
+      <!-- .container -->
+      <b-container>
+        <b-tabs content-class="mt-3">
+          <b-tab title="Step 1" active>
             <form role="form" action="" method="post" class="step-1">
-                <!-- step-1 -->
-                <h3>곶감 등록</h3>
-                <b-row class="setup-content justify-content-end" id="step-1">
-                    <!-- step-1-1 -->
-                    <b-col sm="12" md="6" class="step-1-1">
-                        <b-col md="12">
-                            <div class="img_uploader_wrap">
-                                <label for="fileInput" slot="upload-label">
-                                    <!-- <figure>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                                            <path class="path1" d="M9.5 19c0 3.59 2.91 6.5 6.5 6.5s6.5-2.91 6.5-6.5-2.91-6.5-6.5-6.5-6.5 2.91-6.5 6.5zM30 8h-7c-0.5-2-1-4-3-4h-8c-2 0-2.5 2-3 4h-7c-1.1 0-2 0.9-2 2v18c0 1.1 0.9 2 2 2h28c1.1 0 2-0.9 2-2v-18c0-1.1-0.9-2-2-2zM16 27.875c-4.902 0-8.875-3.973-8.875-8.875s3.973-8.875 8.875-8.875c4.902 0 8.875 3.973 8.875 8.875s-3.973 8.875-8.875 8.875zM30 14h-4v-2h4v2z"></path>
-                                        </svg>
-                                    </figure> -->
-                                    <span class="upload-caption">{{ hasImage ? "Replace" : "Click to upload" }}</span>
-                                </label>
-                                <image-uploader
-                                :preview="true"
-                                :className="['fileinput', { 'fileinput--loaded': hasImage }]" capture="environment"
-                                :debug="1"
-                                doNotResize="gif"
-                                :autoRotate="true"
-                                outputFormat="verbose"
-                                @input="setImage"
-                                >
-                                </image-uploader>
-                            </div>
-
-                            <b-row class="my-1">
-                                <b-col cols="3">
-                                <label for="input-default">곶감 제목:</label>
-                                </b-col>
-                                <b-col cols="9">
-                                <b-form-input id="input-default" required="required" maxlength="100" placeholder="제목을 입력해주세요..."></b-form-input>
-                                </b-col>
-                            </b-row>
-
-                            <b-row class="mt-2">
-                                <b-col cols="12">
-                                    <label for="textarea-default">곶감 스토리(내용):</label>
-                                </b-col>
-                                <b-col cols="12">
-                                    <b-form-textarea required="required" id="textarea-default" class="gotgamStory" placeholder="회원들과 곶감을 공유해주세요..."></b-form-textarea>
-                                </b-col>
-                            </b-row>
-                        </b-col>
-                    </b-col>
-
-                    <!-- step-1-2 -->
-                    <b-col sm="12" md="6" class="step-1-2">
-                        <b-col md="12">
-                            <b-row class="my-3">
-                                <b-col cols="12">
-                                <label for="input-default">인원수</label>
-                                </b-col>
-                                <b-col cols="12">
-                                <b-form-input id="input-default" required="required" maxlength="100" placeholder="몇명이서 갔나요?"></b-form-input>
-                                </b-col>
-                            </b-row>
-                            <b-row class="my-3">
-                                <b-col cols="12">
-                                <label for="input-default">여행 일수</label>
-                                </b-col>
-                                <b-col cols="12">
-                                <b-form-input type="date" id="input-default" required="required" maxlength="100" placeholder="몇명이서 갔나요?"></b-form-input>
-                                </b-col>
-                            </b-row>
-                            <b-row class="my-3">
-                                <b-col cols="12">
-                                <label for="input-default">기분</label>
-                                </b-col>
-                                <b-col cols="12">
-                                <b-form-input id="input-default" required="required" maxlength="100" placeholder="너의 기분은?"></b-form-input>
-                                </b-col>
-                            </b-row>
-                            <b-row class="my-3">
-                                <b-col cols="12">
-                                    <label for="input-default">날씨 선택</label>
-                                </b-col>
-                                <b-col cols="12">
-                                    <b-form-select v-model="selected" :options="options"></b-form-select>
-                                </b-col>
-                            </b-row>
-                            <!-- <button class="btn btn-primary nextBtn btn-lg pull-right" type="button">Next</button> -->
-                        </b-col>
-                    </b-col>
-                    <div class="col-xs-3 col-md-6 button_Wrap">
-                        <div class="col-12">
-                            <button @click.prevent="newTab" class="btn btn-primary nextBtn btn-lg pull-right w-100" type="button">Next</button>
-                        </div>
+              <!-- step-1 -->
+              <h3>곶감 등록</h3>
+              <b-row class="setup-content justify-content-end" id="step-1">
+                <!-- step-1-1 -->
+                <b-col sm="12" md="6" class="step-1-1">
+                  <b-col md="12">
+                    <div class="img_uploader_wrap">
+                      <label for="fileInput" slot="upload-label">
+                        <span class="upload-caption">{{ hasImage ? "Replace" : "Click to upload" }}</span>
+                      </label>
+                      <image-uploader :preview="true" :className="['fileinput', { 'fileinput--loaded': hasImage }]"
+                        capture="environment" :debug="1" doNotResize="gif" :autoRotate="true" outputFormat="verbose"
+                        @input="setImage">
+                      </image-uploader>
                     </div>
-                    
-                </b-row>
 
-                <b-row class="setup-content" id="step-2">
+                    <b-row class="my-1">
+                      <b-col cols="3">
+                        <label for="input-default">곶감 제목:</label>
+                      </b-col>
+                      <b-col cols="9">
+                        <b-form-input id="input-default" required="required" maxlength="100"
+                          placeholder="제목을 입력해주세요..."></b-form-input>
+                      </b-col>
+                    </b-row>
+
+                    <b-row class="mt-2">
+                      <b-col cols="12">
+                        <label for="textarea-default">곶감 스토리(내용):</label>
+                      </b-col>
+                      <b-col cols="12">
+                        <b-form-textarea required="required" id="textarea-default" class="gotgamStory"
+                          placeholder="회원들과 곶감을 공유해주세요..."></b-form-textarea>
+                      </b-col>
+                    </b-row>
+                  </b-col>
+                </b-col>
+
+                <!-- step-1-2 -->
+                <b-col sm="12" md="6" class="step-1-2">
+                  <b-col md="12">
+                    <b-row class="my-3">
+                      <b-col cols="12">
+                        <label for="input-default">인원수</label>
+                      </b-col>
+                      <b-col cols="12">
+                        <b-form-input id="input-default" required="required" maxlength="100" placeholder="몇명이서 갔나요?">
+                        </b-form-input>
+                      </b-col>
+                    </b-row>
+                    <b-row class="my-3">
+                      <b-col cols="12">
+                        <label for="input-default">여행 일수</label>
+                      </b-col>
+                      <b-col cols="12">
+                        <b-form-input type="date" id="input-default" required="required" maxlength="100"
+                          placeholder="몇명이서 갔나요?"></b-form-input>
+                      </b-col>
+                    </b-row>
+                    <b-row class="my-3">
+                      <b-col cols="12">
+                        <label for="input-default">기분</label>
+                      </b-col>
+                      <b-col cols="12">
+                        <b-form-input id="input-default" required="required" maxlength="100" placeholder="너의 기분은?">
+                        </b-form-input>
+                      </b-col>
+                    </b-row>
+                    <b-row class="my-3">
+                      <b-col cols="12">
+                        <label for="input-default">날씨 선택</label>
+                      </b-col>
+                      <b-col cols="12">
+                        <b-form-select v-model="selected" :options="options"></b-form-select>
+                      </b-col>
+                    </b-row>
+                  </b-col>
+                </b-col>
+                <div class="col-xs-3 col-md-6 button_Wrap">
+                  <div class="col-12">
+                    <b-button class="nextBtn w-100" type="button">Save</b-button>
+                  </div>
+                </div>
+
+              </b-row>
+            </form>
+          </b-tab>
+          <b-tab title="Step 2">
+            <form action="">
+              <b-row class="setup-content" id="step-2">
                 <b-col cols="12" offset-md="12">
-                    <b-col md="12">
+                  <b-col md="12">
                     <h3> Step 2 </h3>
                     <b-card no-body>
                       <b-tabs card>
@@ -169,31 +149,28 @@
                           </b-col>
                         </b-tab>
 
-                            <!-- New Tab Button (Using tabs-end slot) -->
-                            <template v-slot:tabs-start>
-                                <b-nav-item @click.prevent="newTab" href="#"><b>+</b></b-nav-item>
-                                <b-nav-item @click.prevent="closeTab" href="#"><b>-</b></b-nav-item>
-                            </template>
+                        <!-- New Tab Button (Using tabs-end slot) -->
+                        <template v-slot:tabs-start>
+                          <b-nav-item @click.prevent="newTab" href="#"><b>+</b></b-nav-item>
+                          <b-nav-item @click.prevent="closeTab" href="#"><b>-</b></b-nav-item>
+                        </template>
 
-                            <!-- Render this if no tabs -->
-                            <template v-slot:empty>
-                                <div class="text-center text-muted">
-                                    <b>+</b> 버튼을 눌러 <br>
-                                    당신의 곶감을 등록해주세요.
-                                </div>
-                            </template>
-                        </b-tabs>
+                        <!-- Render this if no tabs -->
+                        <template v-slot:empty>
+                          <div class="text-center text-muted">
+                            <b>+</b> 버튼을 눌러 <br>
+                            당신의 곶감을 등록해주세요.
+                          </div>
+                        </template>
+                      </b-tabs>
                     </b-card>
                     <b-row class="button_Wrap">
-                        <b-col cols="5" size="lg" class="pull-left">
-                            <b-button>Previous</b-button>
-                        </b-col>
-                        <b-col cols="2"></b-col>
-                        <b-col cols="5" size="lg" class="pull-right">
-                            <b-button>Next</b-button>
-                        </b-col>
+                      <b-col cols="6"></b-col>
+                      <b-col cols="6" size="lg" class="pull-right">
+                        <b-button type="submit" class="col-12">Save</b-button>
+                      </b-col>
                     </b-row>
-                    </b-col>
+                  </b-col>
                 </b-col>
               </b-row>
             </form>
@@ -271,46 +248,6 @@ export default {
 }
 
 /* Form */
-.stepwizard-step p {
-    margin-top: 10px;
-}
-.stepwizard-row {
-    display: table-row;
-}
-.stepwizard {
-    display: table;
-    width: 100%;
-    position: relative;
-}
-.stepwizard-step button[disabled] {
-    opacity: 1 !important;
-    filter: alpha(opacity=100) !important;
-}
-.stepwizard-row:before {
-    top: 14px;
-    bottom: 0;
-    position: absolute;
-    content: " ";
-    width: 100%;
-    height: 1px;
-    background-color: #ccc;
-    /* z-order: 0; */
-}
-.stepwizard-step {
-    display: table-cell;
-    text-align: center;
-    position: relative;
-}
-.btn-circle {
-    width: 30px;
-    height: 30px;
-    text-align: center;
-    padding: 6px 0;
-    font-size: 12px;
-    line-height: 1.428571429;
-    border-radius: 15px;
-}
-
 /* .step */
 .step-1-1, .step-1-2 {
     padding-top: 2rem;
