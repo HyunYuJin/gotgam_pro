81be9867023e606bc602845f2d8aed9f3fa18420
decidone <decidone@gmail.com>
2019-11-11 17:59:16 +0900

    지도 DB연동

--- frontend/src/components/regionMap.vue
+++ frontend/src/components/regionMap.vue
@@ -37,12 +37,35 @@
     components: {VueDaumMap},
     data: () => ({
       appKey: config.appKey,
-      center: {lat:33.450701, lng:126.570667},
+      center: {lat:33.450701, lng:127.570667},
       level: 3,
       mapTypeId: VueDaumMap.MapTypeId.NORMAL,
       libraries: [],
       mapObject: null
     }),
+
+    created () {
+      var id = '';
+      if(this.$store.getters.regionId[0] == undefined){
+          console.log('und');
+          // 지정한 지역정보가 없을 때
+          // 1번 서울을 기본값으로 가져옴
+          id = 1;
+      }else{
+          console.log(this.$store.getters.regionId[0]);
+          id = this.$store.getters.regionId[0];
+      }
+
+      this.$http.get(`/api/region/` + id)
+      .then((res) => {
+        this.center.lat = res.data.lat;
+        this.center.lng = res.data.lng;
+        
+        console.log(res.data.lat);
+        console.log(res.data.lng);
+      });
+    },
+
     methods: {
       onLoad (map) {
         // 지도의 현재 영역을 얻어옵니다
