2deb44479054e48469f24196cefefca0087087e8
HyunYuJin <gusdbwls9772@gmail.com>
2019-12-24 00:09:11 +0900

    mainpage setting

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div id="app">
-    <Index></Index>
-    <!-- <router-view/> -->
+    <div class="wrapper">
+        <Header></Header>
+        <div class="container1">
+            <router-view />
+        </div>
+        <Footer></Footer>
+    </div>
   </div>
 </template>
 
 <script>
+import Header from './components/header.vue';
+import Container from './components/container.vue';
+import Footer from './components/footer.vue';
 import Reset from './css/reset.css';
-import Index from './pages/index';
 
 export default {
   name: 'App',
 
-  components : {
-    Index
+  components: {
+      Header,
+      Container,
+      Footer
   }
 }
 </script>
@@ -23,10 +32,43 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
   color: #2c3e50;
-  /* margin-top: 60px; */
   width: 100%;
   height: 100%;
+}
+
+.wrapper {
+    position: relative;
+    width: 100%;
+    min-height: 100%;
+}
+
+.container1 {
+    width: 100%;
+    /* min-height: 100vh; footer 하단 고정 */
+    padding-bottom: 250px;
+    min-height: 100%;
+    height: 100%;
+}
+
+.footer_wrap_menu{
+    padding: 3.4rem 0;
+}
+
+@media (max-width: 768px) {
+    .container1 {
+        width: 100%;
+        /* min-height: 100vh; footer 하단 고정 */
+        padding-bottom: 315px;
+        min-height: 100%;
+        height: 100%;
+    }
+    .footer_wrap_menu_intro{
+        text-align: center;
+    }
+    .footer_wrap_menu
+    {
+        padding: 2rem 0;
+    }
 }
 </style>
