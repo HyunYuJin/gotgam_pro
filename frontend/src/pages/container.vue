719af4876a9e3e85f3fb09d609a50ade176824af
HyunYuJin <gusdbwls9772@gmail.com>
2019-12-24 11:35:08 +0900

    Combine regionTravel

---
+++ frontend/src/pages/container.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+	  <HomeHeader></HomeHeader>
+	  <div class="wrap">
+		<Section1></Section1>
+		<Section2></Section2>
+		<Section3></Section3>
+		<Section4></Section4>
+	  </div>	
+  </div>
+</template>
+
+<script>
+import HomeHeader from '../components/homeHeader.vue';
+import Section1 from '../components/section1.vue';
+import Section2 from '../components/section2.vue';
+import Section3 from '../components/section3.vue';
+import Section4 from '../components/section4.vue';
+
+export default {
+	components: {
+		HomeHeader,
+		Section1,
+		Section2,
+		Section3,
+		Section4
+	}
+}
+</script>
+
+<style lang="less">
+.wrap {
+	width: 100%;
+	margin: 0 auto;
+}
+</style>
--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,9 +1,12 @@
-import MainPage from '../components/container.vue';
-import Login from '@/components/login.vue';
-import RegionTravel from '@/components/regionTravel.vue';
+// pages
+import MainPage from '@/pages/container.vue';
+import RegionTravel from '@/pages/regionTravel.vue';
+import GotgamDetail from '@/pages/gotgamDetail.vue';
+
+// components
 import MoodTravel from '@/components/moodTravel.vue';
 import Registration from '@/components/registration.vue';
-import GotgamDetail from '@/components/gotgamDetail.vue';
+import Login from '@/components/login.vue';
 import About from '@/components/about.vue';
 import SignUp from '@/components/SignUp.vue';
 import myPage from '@/components/mypage.vue';
@@ -22,46 +25,55 @@ export default [
         name: 'Login',
         component: Login
     },
+
     {
         path: '/regiontravel',
         name: 'RegionTravel',
         component: RegionTravel
     },
+
     {
         path: '/moodtravel',
         name: 'MoodTravel',
         component: MoodTravel
     },
+
     {
         path: '/registration',
         name: 'Registration',
         component: Registration
     },
+
     {
         path: '/regiontravel:id',
         name: 'GotgamDetail',
         component: GotgamDetail
     },
+
     {
         path: '/about',
         name: 'About',
         component: About
     },
+
     {
         path: '/signUp',
         name: 'SignUp',
         component: SignUp
     },
+
     {
         path: '/mypage',
         name: 'myPage',
         component: myPage
     },
+
     {
         path: '/mypage:id',
         name: 'myPageDetail',
         component: myPageDetail
     },
+
     {
         path: '/myPageUpdate',
         name: 'myPageUpdate',
